394b771c9e637331dd0a872751f9b12472287894
SRK.Lyu <superalsrk@gmail.com>
2017-09-07 15:58:14 +0800

    some helper

---
+++ sync/utils/config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+ROOT_PATH = os.path.split(os.path.dirname(os.path.dirname(__file__)))[0]
+STATIC_PATH = os.path.join(ROOT_PATH, 'static')
+TEMPLATE_PATH = os.path.join(ROOT_PATH, 'tmpl')
